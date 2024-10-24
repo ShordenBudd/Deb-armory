09d6ff157ca0dab3f6183c7e0f45ebcc3b6d5e9b
ShordenBudd <dmitrishshwrk@gmail.com>
2024-10-25 02:46:43 +0300

    Add files via upload

---
+++ secure.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#---v-1.0-unstable
+
+echo "░▒▓███████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░░▒▓██████▓▒░░▒▓██████████████▓▒░       ░▒▓████████▓▒░▒▓████████▓▒░░▒▓██████▓▒░░▒▓██████████████▓▒░              ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░ ";
+echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ";
+echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ";
+echo "░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░             ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓██████▓▒░   ";
+echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░                   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ";
+echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░                   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ";
+echo "░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░         ░▒▓█▓▒░    ░▒▓█████████████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ ";
+echo "                                                                                                                                                                                                                                                                        ";
+echo "                                                                                                                                                                                                                                                                        ";
+
+#---Update
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install iptables -y
+touch /etc/iptables.rules
+echo "Updated. Performing iptables configuring..."
+
+#---Backup
+
+mkdir /iptables
+iptables-save > /iptables/before.iptables
+
+#---Drop PING
+
+iptables -A INPUT -p icmp --icmp-type echo-request -j LOG --log-prefix "PING detected: "
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+
+#---Drop portscan
+
+iptables -N PSCAN
+iptables -A PSCAN -j LOG --log-level 4 --log-prefix 'TCP_FLOOD '
+iptables -A PSCAN -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+iptables -A PSCAN -p tcp -m state --state NEW -m recent --set --name TCP_FLOOD
+iptables -A PSCAN -j DROP
+
+iptables -N UPSCAN
+iptables -A UPSCAN -j LOG --log-level 4 --log-prefix 'UDP_FLOOD '
+iptables -A UPSCAN -p udp --udp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+iptables -A UPSCAN -p udp -m state --state NEW -m recent --set --name UDP_FLOOD
+iptables -A UPSCAN -j DROP
+
+iptables -N LOGPSCAN
+iptables -A LOGPSCAN -p tcp --syn -m limit --limit 2000/hour -j RETURN
+iptables -A LOGPSCAN -m limit --limit 200/hour -j LOG --log-prefix "DROPPED Port scan: "
+iptables -A LOGPSCAN -j DROP
+
+iptables -A INPUT -p tcp --syn -j LOGPSCAN
+iptables -A INPUT -p tcp --syn -j NMAPSCAN
+iptables -A INPUT -p udp --syn -j NMAPSCAN
+
+iptables -A INPUT -m recent --name TCP_FLOOD --rcheck --seconds 86400 -j PSCAN
+iptables -A INPUT -m recent --name UDP_FLOOD --rcheck --seconds 86400 -j UPSCAN
+
+#iptables -A INPUT -m recent --name PSCAN --remove
+#iptables -A INPUT -m recent --name UPSCAN --remove
+
+#---Drop SSH-bruteforce
+
+iptables -N SSHBRUTE
+iptables -A SSHBRUTE -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+iptables -A SSHBRUTE -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+iptables -A INPUT -p tcp --syn -j SSHBRUTE
+
+#---Drop invalid packets
+
+iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+
+#---Drop CURL and WGET
+
+sudo iptables -A OUTPUT -p tcp --dport 80 -j DROP
+sudo iptables -A OUTPUT -p tcp --dport 443 -j DROP
+
+#---Save running config
+
+iptables-save > /iptables/after.iptables
+
+#---KillSwitch
+
+get_choice() {
+    echo "Restore to default?"
+    echo "F - Finish"
+    echo "R - Restore"
+    echo "D - Drop"
+    read -t 60 choice
+}
+while true; do
+    get_choice
+
+    if [ -z "$choice" ]; then
+        echo "No input detected. Automatically choosing option D (Drop)."
+        choice=D
+    fi
+
+    case "$choice" in
+        C)
+            echo "Iptables configured. Backup stored in /iptables direcory"
+            break
+            ;;
+        
+        D)  echo "Dropping iptables..."
+            iptables -F
+            iptables -F -t mangle
+            iptables -X
+            iptables -P INPUT ACCEPT
+            iptables -P OUTPUT ACCEPT
+            iptables -P FORWARD ACCEPT
+            break
+            ;;
+
+        R)
+            echo "Restoring..."
+            iptables-restore < /iptables/before.iptables
+            echo "Iptables restored"
+            break
+            ;;
+        *)
+            echo "Invalid choice."
+            ;;
+    esac
+done
